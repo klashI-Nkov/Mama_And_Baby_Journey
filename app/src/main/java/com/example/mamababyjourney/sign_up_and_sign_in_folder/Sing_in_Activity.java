2bb3ab8cebf8a9170737ed316630b313109fac68
klashI-Nkov <albashaklashinkov@gmail.com>
2023-07-08 12:11:07 +0300

    By mohammed issa in 9/6/23 AT 3:26 AM

--- app/src/main/java/com/example/mamababyjourney/sign_up_and_sign_in_folder/Sing_in_Activity.java
+++ app/src/main/java/com/example/mamababyjourney/sign_up_and_sign_in_folder/Sing_in_Activity.java
@@ -1,14 +1,13 @@
 package com.example.mamababyjourney.sign_up_and_sign_in_folder;
 
 import com.example.mamababyjourney.databinding.ActivitySignUpAndSignInFolderSingInActivityBinding;
-import com.example.mamababyjourney.mother_section.Mother_Activity;
 import com.example.mamababyjourney.sign_up_and_sign_in_folder.Info_page.Doctor_Data_Activity;
+import com.example.mamababyjourney.mother_section.Mother_Activity;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import androidx.activity.result.contract.ActivityResultContracts;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import androidx.activity.result.ActivityResultLauncher;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -16,124 +15,108 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import androidx.core.content.ContextCompat;
-import android.app.ProgressDialog;
 import android.content.res.ColorStateList;
 import com.google.android.gms.tasks.Task;
 import android.annotation.SuppressLint;
 import com.example.mamababyjourney.R;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
+import androidx.annotation.Nullable;
 import android.view.WindowManager;
+import android.app.ProgressDialog;
 import android.widget.TextView;
 import android.content.Intent;
 import android.view.ViewGroup;
 import android.graphics.Color;
-import android.app.Activity;
+import java.util.Objects;
 import android.view.View;
+import android.os.Bundle;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
-import android.os.Bundle;
-import android.widget.Toast;
 
-@SuppressWarnings ( { "ConstantConditions" , "FieldMayBeFinal" } )
+@SuppressWarnings ( { "ConstantConditions" , "FieldMayBeFinal" , "deprecation" } )
 @SuppressLint ( "ClickableViewAccessibility" )
 public class Sing_in_Activity extends AppCompatActivity
 {
     ActivitySignUpAndSignInFolderSingInActivityBinding binding ;
 
-    Intent intent ;
+    String password , email ;
 
-    String password , email;
-
+    // هون انا بمعل زي ما تقولي مسج بتظهر للمستخدم بتقله الرجاء الانتظار يعني يستنى لحد ما تتخزن الداتا في الفاير بيس
+    ProgressDialog progressDialog ;
 
     @Override
     protected void onCreate ( Bundle savedInstanceState )
     {
-        super . onCreate ( savedInstanceState ) ;
+        super . onCreate (savedInstanceState ) ;
 
         getWindow ( ) . setFlags (WindowManager . LayoutParams . FLAG_LAYOUT_NO_LIMITS ,WindowManager . LayoutParams . FLAG_LAYOUT_NO_LIMITS ) ;
         Objects . requireNonNull (getSupportActionBar ( ) ) . hide ( ) ;
 
-        binding = ActivitySignUpAndSignInFolderSingInActivityBinding . inflate ( getLayoutInflater ( ) ) ;
+        binding = ActivitySignUpAndSignInFolderSingInActivityBinding . inflate (getLayoutInflater ( ) ) ;
         setContentView ( binding . getRoot ( ) ) ;
+
+        // هون انا بحدد شو المسج الي رح تظهر
+        progressDialog = new ProgressDialog (this ) ;
+        progressDialog . setMessage ( "يرجى الانتظار" ) ;
     }
 
+    // هاد بتنفذ لما اضغط على زر تسجيل الدخول
     public void sing_In_BTN ( View view )
     {
         password = binding . passwordEditText . getText ( ) + "" ;
         email    = binding . EmailEditText    . getText ( ) + "" ;
 
-        // هون انا بمعل زي ما تقولي مسج بتظهر للمستخدم بتقله الرجاء الانتظار يعني يستنى لحد ما تتخزن الداتا في الفاير بيس
-        final ProgressDialog progressDialog = new ProgressDialog (this ) ;
-        progressDialog . setMessage ( "يرجى الانتظار" ) ;
         progressDialog . show ( ) ;
 
-        FirebaseAuth . getInstance ( ) . fetchSignInMethodsForEmail (email ) . addOnCompleteListener (task ->
+        FirebaseAuth . getInstance ( )
+        .fetchSignInMethodsForEmail (email )
+        .addOnCompleteListener (task ->
         {
             if ( ! task . getResult ( ) . getSignInMethods ( ) . isEmpty ( ) )
             {
-
-                List < String > collection_Names = Arrays  . asList ( "Doctors" , "Mothers" );
-
-
-                for ( String collection_Name : collection_Names )
+                FirebaseAuth . getInstance ( )
+                .signInWithEmailAndPassword (email ,password )
+                .addOnCompleteListener (this ,Task ->
                 {
-                    FirebaseFirestore . getInstance ( ) . collection ( collection_Name ) . document ( email ) . get ( ) . addOnSuccessListener ( documentSnapshot ->
-                    {
-                        if ( documentSnapshot . exists ( ) )
-                        {
+                    if ( Task . isSuccessful ( ) )
+                        Get_Data ( ) ;
+                })
+                .addOnFailureListener ( e ->
+                {
+                    progressDialog . dismiss ( ) ;
 
-                            String collection = documentSnapshot . getReference ( ) . getParent ( ) . getId ( );
-
-
-                            if ( documentSnapshot . get ( "صفة المستخدم" ) . toString ( ) .equals ( "طبيب" ) )
-                            {
-                                intent = new Intent (this , Doctor_Data_Activity. class ) ;
-                            }
-
-                            if ( documentSnapshot . get ( "صفة المستخدم" ) . toString ( ) .equals ( "ام" ) )
-                            {
-                                intent = new Intent (this , Mother_Activity. class ) ;
-                            }
-
-
-                            progressDialog . dismiss ( ) ;
-
-                            //startActivity ( intent ) ;
-
-                        }
-                    });
-                }
-
-
+                    if ( e . getMessage ( ) . equals ( "The password is invalid or the user does not have a password." ) )
+                        Snack_Bar ("كلمة المرور خاطئه يرجى التاكد من كلمة المرور و المحاوله مره اخرى" ) ;
+                });
             }
             else
             {
                 progressDialog . dismiss ( ) ;
-                Toast . makeText ( this , "لايوجد حساب مرتبط بهذا الايميل"  , Toast . LENGTH_LONG ) .show ( ) ;
+                Snack_Bar ("لايوجد حساب مرتبط بهذا الايميل" );
             }
         });
-
     }
 
     // هاد بتنفذ لما اضغط على تسجيل الدخول باستخدام جوجل
     public void Sing_In_By_Google ( View view)
     {
+        progressDialog . show ( ) ;
 
-        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions . Builder (GoogleSignInOptions . DEFAULT_SIGN_IN ) . requestIdToken ( getString (R . string . default_web_client_id ) ) . requestEmail ( ) . build ( ) ;
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions . Builder (GoogleSignInOptions . DEFAULT_SIGN_IN )
+        .requestIdToken ( getString (R . string . default_web_client_id ) ) . requestEmail ( ) . build ( ) ;
+
         GoogleSignInClient mClient = GoogleSignIn . getClient (this ,googleSignInOptions ) ;
 
         // login
         Intent intent = mClient . getSignInIntent ( ) ;
-        activityResultLauncher . launch (intent ) ;
+        startActivityForResult ( intent , 2 );
     }
 
-    private ActivityResultLauncher < Intent > activityResultLauncher = registerForActivityResult (new ActivityResultContracts . StartActivityForResult ( ) ,result ->
+    public void onActivityResult ( int requestCode , int resultCode , @Nullable Intent data )
     {
-        if ( result . getResultCode ( ) == Activity . RESULT_OK )
+        super . onActivityResult (requestCode ,resultCode ,data ) ;
+
+        if ( resultCode == RESULT_OK  )
         {
-            Intent data = result . getData ( ) ;
             Task < GoogleSignInAccount > task = GoogleSignIn . getSignedInAccountFromIntent (data ) ;
 
             try
@@ -142,11 +125,16 @@ public class Sing_in_Activity extends AppCompatActivity
 
                 // auth
                 AuthCredential credential = GoogleAuthProvider . getCredential (account . getIdToken ( ) ,null ) ;
-                FirebaseAuth . getInstance ( ) . signInWithCredential (credential ) . addOnCompleteListener (this ,Task ->
+
+                FirebaseAuth . getInstance ( )
+                .signInWithCredential (credential )
+                .addOnCompleteListener (this ,Task ->
                 {
                     if ( Task . isSuccessful ( ) )
                     {
-                        Snack_Bar (FirebaseAuth . getInstance ( ) . getCurrentUser ( ) . getDisplayName ( ) ) ;
+                        email = FirebaseAuth . getInstance ( ) . getCurrentUser ( ) . getEmail ( ) ;
+
+                        Get_Data ( ) ;
                     }
                 });
             }
@@ -155,7 +143,10 @@ public class Sing_in_Activity extends AppCompatActivity
                 throw new RuntimeException ( a ) ;
             }
         }
-    });
+    }
+
+
+
 
     /*
       هاد الفنكشن مستعمله عشان اعرض مسج معين و مستعمله في معظم كلاسات التطبيق
@@ -185,11 +176,11 @@ public class Sing_in_Activity extends AppCompatActivity
             لكن انا هو مش مستعمل لا LENGTH_LONG ولا مستعمل LENGTH_SHORT حاكي اليه مباشره المده هي 7000 ملي ثانيه يعني 5
             ثواني لانه كل 1000 ملي ثانيه بتساوي ثانيه وحده
         */
-        Snackbar snackbar = Snackbar . make ( binding . Constraint , Message , 7000 ) ;
+        Snackbar snackbar = Snackbar . make (binding . Constraint ,Message ,7000 ) ;
 
         // السطر الي تحت عشان اغير لون خلفية ال Snackbar
 
-        int color = Color.parseColor ( "#292929" ) ;
+        int color = Color.parseColor ("#292929" ) ;
 
         snackbar . getView ( ) . setBackgroundTintList ( ColorStateList . valueOf (color ) ) ;
 
@@ -201,13 +192,13 @@ public class Sing_in_Activity extends AppCompatActivity
              في حجم و لون الخط تبعها
          */
         View snackbarView = snackbar . getView ( ) ;
-        TextView textView = snackbarView . findViewById ( com . google . android . material . R . id . snackbar_text ) ;
+        TextView textView = snackbarView . findViewById (com . google . android . material . R . id . snackbar_text ) ;
 
         // هاد السطر قلنا اله انه ما بدي كل المسج تظهر في سطر واحد بدي لو كانت اكثر من سطر تنعرض مثل ما هي مش كلها في سطر واحد
         textView . setSingleLine ( false ) ;
 
         // هون غيرنا لون الخط للتكست
-        textView . setTextColor ( ContextCompat . getColor ( this , R . color . white ) ) ;
+        textView . setTextColor ( ContextCompat . getColor (this ,R . color . white ) ) ;
 
         // هون غيرنا حجم الخط
         textView . setTextSize ( 15 ) ;
@@ -256,5 +247,68 @@ public class Sing_in_Activity extends AppCompatActivity
         // و هاد السطر الي بعمله هو انه بظهر الي ال snackbar
         snackbar . show ( ) ;
 
+    }
+
+    // هاد الفنكشن الي بجيب داتا المستخدم بس يعمل تسجيل دخول
+    private void Get_Data ( )
+    {
+        // هون عندي list مخزن فيها اسماء ال collections الي فيها بيانات الدكاتره و الامهات عشان باستعمال ال for each الي تحت الف عليهم لحتى ادور على الدكيومنت تبع المستخدم
+        List < String > collection_Names = Arrays  . asList ( "Doctors" , "Mothers" );
+
+        for ( String collection_Name : collection_Names )
+        {
+            // هون اول شي بقله دور الي على ال collection الي فيها ال document تبع المستخدم
+            FirebaseFirestore . getInstance ( )
+            .collection (collection_Name )
+            .get ( ) . addOnCompleteListener (task ->
+            {
+                // هون اذا كانت ال collection الي بدور عليها موجوده بقله خش ابحث عن document المستخدم فيها
+                if ( task . isSuccessful ( ) )
+                {
+                    FirebaseFirestore . getInstance ( )
+                    .collection (collection_Name )
+                    .document   (email )
+                    .get ( ) . addOnSuccessListener (documentSnapshot ->
+                    {
+                        if ( documentSnapshot . exists ( ) )
+                        {
+                            // هون اذا كان المستخدم طبيب او طبيبه بخلي التطبيق يوديه على قسم الدكتور
+                            if ( documentSnapshot . get ( "صفة المستخدم" ) . toString ( ) . equals ( "طبيب" ) || documentSnapshot . get ( "صفة المستخدم" ) . toString ( ) . equals ( "طبيبة" ) )
+                            {
+                                // هون بوجهو لشاشة الدكتور
+                                Intent intent = new Intent (Sing_in_Activity . this ,Doctor_Data_Activity . class ) ;
+
+                                // هون بفهم شاشة الدكتور اني جاي من شاشة تسجيل الدخول
+                                intent . putExtra ("action" ,"signing" ) ;
+
+                                // هون بخفي الاشعار الي بحكي يرجى الانتظار
+                                progressDialog . dismiss ( ) ;
+
+                                // هون بنتقل لشاشة الدكتور
+                                startActivity (intent ) ;
+                                finish ( ) ;
+                            }
+
+                            // هون اذا كان المستخدم طبيب او طبيبه بخلي التطبيق يوديه على قسم الام
+                            if ( documentSnapshot . get ( "صفة المستخدم" ) . toString ( ) . equals ( "أم" ) )
+                            {
+                                // هون بوجهو لشاشة الام
+                                Intent intent = new Intent (Sing_in_Activity . this ,Mother_Activity . class ) ;
+
+                                // هون بفهم شاشة الام اني جاي من شاشة تسجيل الدخول
+                                intent . putExtra ("action" ,"signing" ) ;
+
+                                // هون بخفي الاشعار الي بحكي يرجى الانتظار
+                                progressDialog . dismiss ( ) ;
+
+                                // هون بنتقل لشاشة الام
+                                startActivity (intent ) ;
+                                finish ( ) ;
+                            }
+                        }
+                    });
+                }
+            });
+        }
     }
 }
